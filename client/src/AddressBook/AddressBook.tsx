78a4db0d2e70f5614915fdfad4c6585a915c5af1
thefattestcat <48980192+thefattestcat@users.noreply.github.com>
2021-06-16 15:39:07 +1200

    initial commit

---
+++ client/src/AddressBook/AddressBook.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function AddressBook() {
+  return (
+    <div className="AddressBook">
+      
+    </div>
+  );
+}
+
+export default AddressBook;
