e916b221207fe0b90d8e03c27c3941ab1df2202e
thefattestcat <48980192+thefattestcat@users.noreply.github.com>
2021-06-25 12:35:30 +1200

    Profile

---
+++ client/src/ContactSearchField/ContactSearchField.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Divider, InputAdornment, TextField } from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search';
+
+const ContactSearchField: React.FC<any> = props => {
+    const [value, setValue] = useState<any>('');
+
+    const handleSubmit = () => {
+        props.onClick(value);
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    label="Contact"
+                    placeholder="Search, name, address (0x), or ENS"
+                    helperText="Full width!"
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
+            </form>
+        </>)
+}
+
+export default ContactSearchField;
